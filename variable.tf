f39fffc7d8b2e39f3bd75dcc8aa718c78648dc13
Amine Ballalou <ballalouahmedamine@gmail.com>
2022-07-05 12:21:41 +0000

    Code review v1 init

--- variable.tf
+++ variable.tf
@@ -1,19 +1,23 @@
 variable "key_name" {
+  description = "The name for the key pair to be generated"
   default = "test-key"
   type    = string
 }
 
 variable "environment" {
+  description = "Environment identifier for the key pair"
   default = "dev"
   type    = string
 }
 
 variable "ssm_parameter" {
+  description = "The name of the ssm parameter for storing the keypair"
   default = "/test/ssm"
   type    = string
 }
 
 variable "region" {
+  description = "Specify the region in which key needs to be generate"
   default = "us-east-2"
   type    = string
 }
