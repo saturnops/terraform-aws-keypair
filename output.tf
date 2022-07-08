88accf0e46293ff6e94acfb4d8f3fe179e05acf6
Amine Ballalou <ballalouahmedamine@gmail.com>
2022-07-08 15:38:32 +0530

    removed numeric part from output

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
-output "_1_tls_private_key" {
+output "tls_private_key" {
   description = "Warning!! ! Please Save the private key data for future use !"
   value       = nonsensitive(tls_private_key.aws_keypair.private_key_pem)
 }
 
-output "_2_public_key" {
+output "public_key" {
   description = "The Public key data"
   value       = tls_private_key.aws_keypair.public_key_openssh
 }
 
-output "_3_key_pair_key_name" {
+output "key_pair_key_name" {
   description = "The key pair name."
   value       = module.key_pair.key_pair_key_name
 }
